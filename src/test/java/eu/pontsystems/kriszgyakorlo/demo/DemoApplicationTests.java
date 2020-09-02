653ce739bd339a448cf40b2f50b9034b9db2d297
klucsik <klucsik.krisztian@gmail.com>
2020-09-02 11:54:32 +0200

    initial commit - basic mvc webpage served

---
+++ src/test/java/eu/pontsystems/kriszgyakorlo/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.pontsystems.kriszgyakorlo.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
