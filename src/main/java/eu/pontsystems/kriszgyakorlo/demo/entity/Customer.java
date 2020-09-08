5e071437242229bdfcdcc264df47e57bb7028ea1
klucsik <klucsik.krisztian@gmail.com>
2020-09-08 09:01:08 +0200

    lombokize

--- src/main/java/eu/pontsystems/kriszgyakorlo/demo/entity/Customer.java
+++ src/main/java/eu/pontsystems/kriszgyakorlo/demo/entity/Customer.java
@@ -1,5 +1,7 @@
 package eu.pontsystems.kriszgyakorlo.demo.entity;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
@@ -8,6 +10,8 @@ import javax.validation.constraints.NotBlank;
 import java.time.LocalDateTime;
 
 @Entity
+@Getter
+@Setter
 public class Customer {
 
     @Id
@@ -35,38 +39,6 @@ public class Customer {
     @Override
     public String toString() {
         return String.format("Customer[id=%d, firstName='%s', lastName='%s']", id, firstName, lastName);
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public LocalDateTime getLastUpdatedAt() {
-        return lastUpdatedAt;
     }
 
 }
